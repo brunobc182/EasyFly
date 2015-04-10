4bcdfebc7b7453bf61b83cd50ecb632d4dc282e2
Bruno Braga Chagas <bbchagas@gmail.com>
2015-04-10 18:19:08 -0300

    Add Imagens
    Ads imagens e mudanças de funções.

--- game.lua
+++ game.lua
@@ -2,7 +2,7 @@ local composer = require("composer")
 local scene = composer.newScene( )
 local physycs = require( "physics")
 physics.start( )
-physics.setDrawMode( "hybrid")
+--physics.setDrawMode( "hybrid")
 
 local player
 local bg1
@@ -15,8 +15,9 @@ local up = false --determinina se o player vai para cima
 local blockTime -- tempo do block
 local speed = 1 -- velocidade os obstaculos
 local tm --cancelar a criação de Blocos
-local speedTm -- amumentar velocidade dos passaros
+local speedTm -- amumentar velocidade dos blocks
 local numberOfLives = 1
+local yPos = {50, 200, 400}
 
 --add funções
 local bgScroll = {}
@@ -87,17 +88,17 @@ end
 
 function setupBG( )
   --add Imagens do BG
-bg1 = display.newImageRect("image/bg02.png", _W, _H)
-bg1.x = 0 
+bg1 = display.newImageRect("image/bg01.png", _W, _H)
+bg1.x = _W2
 bg1.y = _H2
 scene.view:insert( bg1 )
 
-bg2 = display.newImageRect("image/bg02.png", _W, _H)
+bg2 = display.newImageRect("image/bg01.png", _W, _H)
 bg2.x = bg1.x + _W
 bg2.y = _H2
 scene.view:insert( bg2 )
 
-bg3 = display.newImageRect("image/bg02.png", _W, _H)
+bg3 = display.newImageRect("image/bg01.png", _W, _H)
 bg3.x = bg2.x + _W
 bg3.y = _H2
 scene.view:insert( bg3 )
@@ -108,7 +109,7 @@ physics.addBody( teto, "static" )
 teto.name = "teto"
 scene.view:insert( teto )
 
-piso = display.newRect( _W2, _H, _W+100, 0.1 )
+piso = display.newRect( _W2, _H, _W+100, 1 )
 piso:setFillColor( 0, 0, 0 )
 physics.addBody( piso, "static" )
 piso.name = "piso"
@@ -124,7 +125,7 @@ end
 
 function setupPlayer( )
 
-local options = { width = 50, height = 35, numFrames = 8}
+local options = { width = 50, height = 41, numFrames = 8}
 local playerSheet = graphics.newImageSheet( "image/playerSheet.png", options )
 local sequenceData = {
   { name = "fly", start = 1, count = 8 , time = 1000, loopCount = 0}
@@ -182,13 +183,21 @@ end
 
 --cria os obstaculos
 function createBlocks(event)
- local passaro = display.newImage("image/passaro.png")    
-  passaro.x = _W
-  passaro.y = math.random( _H - 20 )
-  passaro.name = 'passaro'
-  physics.addBody(passaro, "kinematic") 
-  passaro.isSensor = true
-  blocks:insert( passaro )
+
+local options = { width = 70, height = 54, numFrames = 4}
+local playerSheet = graphics.newImageSheet( "image/blocksheet.png", options )
+local sequenceData = {
+  { name = "fly", start = 1, count = 4 , time = 1000, loopCount = 0}
+}
+  local rnd = math.floor(math.random() * 4) + 1
+  block = display.newSprite(playerSheet, sequenceData)    
+  block.x = _W
+  block.y = yPos[math.floor(math.random() * 3)+1]
+  block.name = 'block'
+  physics.addBody(block, "kinematic") 
+  block.isSensor = true
+  block:play( )
+  blocks:insert( block )
 end
 
 function update(event)  
@@ -215,7 +224,7 @@ end
   end
 
 function velocidade()
-  scroll = scroll + 1
+  speed = speed + 1
   --Icon
   local icon = display.newImage('image/speed.png', _W2 , _H2)
   transition.from(icon, {time = 200, alpha = 0.1, onComplete = function() timer.performWithDelay(500, function() 
@@ -229,11 +238,11 @@ function onCollision(event)
     if ( event.phase == "began" ) then
         --SNIP--
         
-        if(event.object1.name == "player" and event.object2.name == "passaro") then            
+        if(event.object1.name == "player" and event.object2.name == "block") then            
             gameOver()
         end
          
-         if(event.object1.name == "passaro" and event.object2.name == "player") then            
+         if(event.object1.name == "block" and event.object2.name == "player") then            
             gameOver()
         end
          if(event.object1.name == "player" and event.object2.name == "teto") then         
